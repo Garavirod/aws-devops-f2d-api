f7246c76d3d046cedd06bb68794b7f31b8144160
Garavirod <rodrigogarciaavila26@gmail.com>
2024-08-08 19:43:53 -0600

    add main and variables files into setup for terraform configuration

---
+++ infra/setup/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hasicorp/aws"
+        version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "devops-tf-state-f2d-project"
+    key = "tf-state-setup"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "devops-tf-lock-f2d-project"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+        Environment = terraform.workspace
+        Project = var.project
+        Contact = var.contact
+        Manageby = "Terraform/setup"
+    }
+  }
+}
---
+++ infra/setup/variables.tf
@@ -0,0 +1,19 @@
+variable "st_state_bucket" {
+  description = "Name of s3 bucket in AWS for storing TF state"
+  default = "devops-tf-state-f2d-project"
+}
+
+variable "tf_state_lock_table" {
+  description = "Name of Dynamo table for storing TF lock"
+  default = "devops-tf-lock-f2d-project"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default = "aws-devops-f2f-api"
+}
+
+variable "contact" {
+  description = "Contact name for taggin resources"
+  default = "me@example.com"
+}
