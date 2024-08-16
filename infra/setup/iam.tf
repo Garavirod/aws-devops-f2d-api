d0cc13c5d0e1068fb17bd38568682e1bcb029d78
Garavirod <rodrigogarciaavila26@gmail.com>
2024-08-16 12:42:20 -0600

    feat(infra): recreate ecr policies

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -77,7 +77,7 @@ resource "aws_iam_user_policy_attachment" "tf_backend" {
 # Policy for ECR access #
 #########################
 
-/* data "aws_iam_policy_document" "ecr" {
+data "aws_iam_policy_document" "ecr" {
   statement {
     effect    = "Allow"
     actions   = ["ecr:GetAuthorizationToken"]
@@ -109,7 +109,7 @@ resource "aws_iam_policy" "ecr" {
 resource "aws_iam_user_policy_attachment" "ecr" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.ecr.arn
-} */
+}
 /*
 #########################
 # Policy for EC2 access #
