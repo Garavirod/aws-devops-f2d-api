dbd73b9f5f19159df36eac4d2ffbb25b30f2c77f
Garavirod <rodrigogarciaavila26@gmail.com>
2024-08-19 17:25:07 -0600

    feat(infra | deploy | ecs): Atach ssm permissions policy to task role

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -26,6 +26,33 @@ resource "aws_iam_role_policy_attachment" "task_execution_role" {
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
 }
 
+// Task definition role
+resource "aws_iam_role" "app_task" {
+  name               = "${local.prefix}-app-task"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+/* 
+A task role is the role that's given to the actual task that's running
+in Fargate after it's been launched.
+
+So this is what's going to allow us to connect into the task that's 
+running in Fargate in order to perform management commands like 
+creating super users or debugging issues.
+*/
+
+resource "aws_iam_policy" "task_ssm_policy" {
+  name        = "${local.prefix}-task-ssm-role-policy"
+  path        = "/"
+  description = "Policy to allow System Manager to execute in container"
+  policy      = file("./templates/ecs/task-ssm-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
+  role       = aws_iam_role.app_task.name
+  policy_arn = aws_iam_policy.task_ssm_policy.arn
+}
+// Cluster definition
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
