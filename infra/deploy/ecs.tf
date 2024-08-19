56a464210f147f9c99994daa9ce840e1124b7a4d
Garavirod <rodrigogarciaavila26@gmail.com>
2024-08-19 17:04:36 -0600

    feat(infra | deploy | ecs): Add task exec role permissions for ECS task

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -2,6 +2,30 @@
 # ECS Cluster for running app on Fargate #
 ##########################################
 
+/* 
+    ECS Task needs permissions for assuming a role (Task role).
+    So, task role needs a policy for assumimg a role and such role needs to have permissions for performing jobs.
+    The role in question needs to have the necessary permissions to help the task perfom certain aws jobs.
+    The permission needs to be defined into a policy and this policy needs to be attached to the task role.
+ */
+
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to logs."
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
