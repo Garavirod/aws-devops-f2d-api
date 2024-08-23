dbd9771ab456c3f601550d1ccb148952dab6efa7
Garavirod <rodrigogarciaavila26@gmail.com>
2024-08-23 12:32:14 -0600

    feat(infra | deploy | ecs): Update ecs service in order to add load balancer

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -228,10 +228,18 @@ resource "aws_ecs_service" "api" {
     assign_public_ip = true
 
     subnets = [
-      aws_subnet.public_a.id, // private for real appp and public for testing 
-      aws_subnet.public_b.id
+      # aws_subnet.public_a.id, // private for real appp and public for testing 
+      # aws_subnet.public_b.id
+      aws_subnet.private_a.id, // resgister the servies in private subnets,, this way ecs task is protected.
+      aws_subnet.private_b.id
     ]
 
     security_groups = [aws_security_group.ecs_service.id]
+  }
+  // load balcner will forward requests to a target group and tg will forward to proxy container runnning on 8000
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api.arn
+    container_name   = "proxy"
+    container_port   = 8000
   }
 }
